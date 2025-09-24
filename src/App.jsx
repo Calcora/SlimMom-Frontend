366b635a6523055d2d36d5464240694775c5f839
Nur Aleyna Pektaş <nuraleynaaaa@gmail.com>
2025-09-25 02:13:17 +0300

    Diary.jsx and css

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import Diary from "./Diary";
 import GramCalc from "./GramCalc";
 import Login from "./Login";
 import MainPage from "./MainPage/MainPage";
@@ -11,6 +12,19 @@ function App() {
       <Login />
       <Register />
       <GramCalc />
+      <Diary
+        products={[
+          { name: "Eggplant", grams: 100, kcalPer100: 320 },
+          { name: "Poultry meat", grams: 100, kcalPer100: 480 },
+        ]}
+        onBack={() => setShowDiary(false)}
+        onExit={() => console.log("Exit")}
+        onMenuClick={() => console.log("Menu")}
+        onAddClick={() => setShowDiary(false)} // + butonu: tekrar GramCalc aç
+        onDelete={(i) =>
+          setProducts((prev) => prev.filter((_, idx) => idx !== i))
+        }
+      />
     </>
   );
 }
