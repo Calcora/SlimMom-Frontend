960f3591e3c11415a46d2aa8a09ce097e9c9c5ba
Nur Aleyna Pektaş <nuraleynaaaa@gmail.com>
2025-09-25 18:29:40 +0300

    mobilde Diary ve GramCalc Appte bağlandı

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,53 @@
-import "./App.css";
+import { useState } from "react";
 import Diary from "./Diary";
 import GramCalc from "./GramCalc";
-import Login from "./Login";
-import MainPage from "./MainPage/MainPage";
-import Register from "./Registration";
 
-function App() {
-  return (
-    <>
-      <MainPage />
-      <Login />
-      <Register />
-      <GramCalc />
-      <Diary
-        products={[
-          { name: "Eggplant", grams: 100, kcalPer100: 320 },
-          { name: "Poultry meat", grams: 100, kcalPer100: 480 },
-        ]}
-        onBack={() => setShowDiary(false)}
-        onExit={() => console.log("Exit")}
-        onMenuClick={() => console.log("Menu")}
-        onAddClick={() => setShowDiary(false)} // + butonu: tekrar GramCalc aç
-        onDelete={(i) =>
-          setProducts((prev) => prev.filter((_, idx) => idx !== i))
-        }
-      />
-    </>
+export default function App() {
+  const [view, setView] = useState("diary");
+  const [products, setProducts] = useState([]);
+  const [date, setDate] = useState(new Date());
+  const dailyRate = 2800;
+
+  const handleAddClick = () => setView("add");
+
+
+  const handleAdd = (item) => {
+    
+    setProducts((prev) => [...prev, item]);
+    setView("diary");
+  };
+
+
+  const handleDelete = (index) => {
+    setProducts((prev) => prev.filter((_, i) => i !== index));
+  };
+
+  
+  const handleBack = () => setView("diary");
+  const handleExit = () => setView("diary");
+  const handleMenuClick = () => {
+    console.log("menu");
+  };
+
+  return view === "diary" ? (
+    <Diary
+      products={products}
+      date={date}
+      dailyRate={dailyRate}
+      onBack={handleBack}
+      onExit={handleExit}
+      onMenuClick={handleMenuClick}
+      onAddClick={handleAddClick}
+      onDelete={handleDelete}
+    />
+  ) : (
+    <GramCalc
+      title="Nic"
+      onMenuClick={handleMenuClick}
+      onBack={() => setView("diary")}
+      onExit={() => setView("diary")}
+      onAdd={handleAdd}
+      pending={false}
+    />
   );
 }
-
-export default App;
