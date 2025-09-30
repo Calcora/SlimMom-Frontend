63d9e61f22112b71689bff7ec6c0ad2fd83ca37e
Mürsel ŞEN <murselsen_35@hotmail.com>
2025-09-30 13:43:32 +0300

    fix: Improve error handling in loginUser operation

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -6,6 +6,18 @@ export const loginUser = createAsyncThunk(
   "auth/loginUser",
   async (_, thunkAPI) => {
     console.log("Login User dispatched");
+    try {
+      const response = await api.post("auth/login", _);
+      console.log("Login response:", response);
+      if (response.status === 200) {
+        return response.data;
+      } else {
+        return thunkAPI.rejectWithValue(response.data.message);
+      }
+    } catch (error) {
+      console.error("Login error:", error);
+      return thunkAPI.rejectWithValue(error.message);
+    }
   }
 );
 export const registerUser = createAsyncThunk(
